03432238da7b0c5dd26fc8a802a25f26461eb90c
hassanmujtaba <hassan03203406130@gmail.com>
2020-06-12 09:16:35 +0500

    project covid 19 completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <>
+      <div className={styles.container}>
+        <h2>COVID-19 Tracker App</h2>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Cards data={data} />
+        <Chart data={data} country={country} />
+      </div>
+      <p className={styles.Copyright}>All Copyright &#169; reserved to <strong>Hassan Mujtaba</strong>.</p>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
